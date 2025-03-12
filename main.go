dc3aa6b6d476d13c8e4e5a4e68fcb5745ae7a13a
Dominik Pall <kurdax@Kurdax>
2025-03-12 19:52:51 +0100

    Add function and test case

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
+	"strings"
 )
 
 func main() {
     fmt.Print("Hello, World!")
+}
+
+func cleanInput(text string) []string {
+  trimmed := strings.Trim(text, " ")
+  split := strings.Split(trimmed, " ")
+  result := make([]string, 0)
+
+  for _, word := range split {
+      result = append(result, strings.ToLower(word))
+  }
+
+  return result
 }
---
+++ repl_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestCleanInput(t *testing.T) {
+  cases := []struct {
+    input string
+    expected []string
+  } {
+    {
+      input: " hello world ", 
+      expected: []string{"hello", "world"},
+    },
+  }
+
+  for _, c := range cases {
+    actual := cleanInput(c.input)
+
+    if len(actual) != len(c.expected) {
+      t.Errorf("Expected and actual slice differ in length, actual %v - expected %v", len(actual), len(c.expected))
+      t.Fail()
+    }
+  
+    for i := range actual {
+      word := actual[i]
+      expected := c.expected[i]
+
+      if word != expected {
+        t.Errorf("expeted word %s didn't match actual %s", expected, word)
+        t.Fail()
+      }
+    }
+  }
+}
