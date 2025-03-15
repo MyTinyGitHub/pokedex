23c950b66e8a6771a38200d55f0f5c24262c28ce
Dominik Pall <kurdax@Kurdax>
2025-03-15 08:58:09 +0100

    Extend pokedex cli

--- main.go
+++ main.go
@@ -7,18 +7,63 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+  name string
+  description string
+  callback func() error
+}
+
+var registry map[string]cliCommand
+
 func main() {
   scanner := bufio.NewScanner(os.Stdout)
+  registry = map[string]cliCommand{
+    "exit": {
+      name: "exit", 
+      description: "Exit the Pokedex",
+      callback: exitCommand,
+    },
+    "help": {
+      name: "help", 
+      description: "Displays a help message",
+      callback: helpCommand,
+    },
+  }
+
+
+  fmt.Println("Welcome to the Pokedex!")
 
   for {
     fmt.Print("Pokedex > ")
     scanner.Scan()
     input := scanner.Text()
-    command := strings.ToLower(input)
-    command = strings.Fields(command)[0]
-    fmt.Printf("Your command was: %v\n", command)
+    if input == "" {
+      exitCommand()
+    }
+    input = strings.Fields(input)[0]
+    input = strings.ToLower(input)
+    command, ok := registry[input]
 
+    if !ok {
+      continue
+    }
+
+    command.callback()
   }
+}
+
+func helpCommand() error {
+  for _, value := range registry {
+      fmt.Printf("%v: %v\n", value.name, value.description)
+  }
+
+  return nil
+}
+
+func exitCommand() error {
+  fmt.Println("Closing the Pokedex... Goodbye!")
+  os.Exit(0)
+  return nil
 }
 
 func cleanInput(text string) []string {
